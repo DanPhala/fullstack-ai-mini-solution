481edac753404839a0382e69855a426cd0f8b8e8
DanPhala <mrphalad@gmail.com>
2025-09-21 20:55:12 +0200

    feat:working predictions

--- controllers/predict_controller.py
+++ controllers/predict_controller.py
@@ -2,16 +2,26 @@ from fastapi import APIRouter, HTTPException, status
 from models.request.ml_predict import TrainPayload
 from services.ml_service.ml_service import MLService
 from models.response.trained_model import TrainResponse
+from models.response.energy_prediction import PredictResponse
+from models.request.energy_prediction import PredictPayload
 
 router = APIRouter()
 ml_service = MLService()
 
-@router.post("/v1/ml/train")
+@router.post("/v1/train")
 async def train_model(payload: TrainPayload) -> TrainResponse:
     if not payload.rows or len(payload.rows) == 0:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="No data provided for training.")
     try:
         model_info = await ml_service.train_and_persist_model(payload.rows)
         return model_info
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
+@router.post("/v1/predict", response_model=PredictResponse)
+async def predict(payload: PredictPayload) -> PredictResponse:
+    try:
+        prediction = await ml_service.predict(payload)
+        return prediction
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
